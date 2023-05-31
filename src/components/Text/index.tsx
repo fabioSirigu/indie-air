7686bd82e7b9e0fea3f7f26fd7e2b94ca692c119
Fabio Sirigu <fabio.sirigu.1995@gmail.com>
2023-05-31 12:25:44 +0200

    feat: add component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,3 +1,19 @@
-export const Text = () => {
-  return <>aaaa</>
-}
+import { memo, PropsWithChildren } from 'react'
+import { FontVariant, FontWeight, ThemeColor } from '../../style/theme'
+import { StyledText } from './styled'
+
+type Props = PropsWithChildren<{
+  variant?: FontVariant
+  color?: ThemeColor
+  weight?: FontWeight
+}>
+
+export const Text = memo(
+  ({ variant = 'span', color = 'text', weight = 'mediumBold', children }: Props) => {
+    return (
+      <StyledText as={variant} weight={weight} color={color}>
+        {children}
+      </StyledText>
+    )
+  }
+)
--- src/components/Text/styled.ts
+++ src/components/Text/styled.ts
@@ -1,3 +1,17 @@
 import styled from 'styled-components'
+import { FontVariant, ThemeColor, FontWeight } from '../../style/theme'
 
-export const StyledText = styled.div``
+type Props = {
+  as: FontVariant
+  color: ThemeColor
+  weight?: FontWeight
+}
+
+export const StyledText = styled.span<Props>`
+  font-size: ${({ theme, as }) => {
+    const textSize = theme.fonts[as].size
+    return theme.fontSizes[textSize]
+  }}rem;
+  color: ${({ theme, color }) => (color ? theme.colors[color] : theme.colors.text)};
+  font-weight: ${({ theme, weight }) => (weight ? theme.fontWeight[weight] : null)};
+`
